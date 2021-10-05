358307d17560056711b52e642ddf061e114a7314
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2021-10-05 16:51:04 +0200

    new function for greeting

--- session1/hello_world/src/main/kotlin/Main.kt
+++ session1/hello_world/src/main/kotlin/Main.kt
@@ -2,6 +2,10 @@ fun main(){
     println("Hello world")
     var person = arrayOf("Hira", "David", "Amina", "Kevser", "Niko")
     for(name in person){
-        println("Hello $name")
+        sayHello(name)
     }}
+
+fun sayHello(name: String){
+    println("Hello $name")
+}
 
