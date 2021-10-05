8a114eff90d76e91458f6af4208f181ffc1c94d2
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2021-10-05 16:46:09 +0200

    for loop in main

--- session1/hello_world/src/main/kotlin/Main.kt
+++ session1/hello_world/src/main/kotlin/Main.kt
@@ -1,11 +1,7 @@
 fun main(){
     println("Hello world")
-    sayHello()
-}
-
-fun sayHello(){
     var person = arrayOf("Hira", "David", "Amina", "Kevser", "Niko")
     for(name in person){
         println("Hello $name")
-    }
-}
+    }}
+
