1743641b7a3eb71afdd5602e4bc2cc55500d3571
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2021-10-07 09:30:50 +0200

    new "error" sentence if you don't fill anything

--- session1/rock_paper_scissors_2/src/main/kotlin/Main.kt
+++ session1/rock_paper_scissors_2/src/main/kotlin/Main.kt
@@ -1,22 +1,24 @@
 fun main(){
     val choices = arrayOf("rock", "paper", "scissors")
     val computerChoice = choices.random()
-    val userChoice = choose(computerChoice)
-
+    choose(computerChoice)
 }
 
 fun choose(computerChoice:String){
     println("Rock, paper or scissors??")
-    val answer = readLine()
-    checkAnswer(answer, computerChoice)
+    val userChoice = readLine()
+    checkAnswer(userChoice, computerChoice)
 }
 
-fun checkAnswer(answer:String?, computerChoice:String){
+fun checkAnswer(userChoice:String?, computerChoice:String){
 
-    if ((answer == "rock") ||(answer == "paper")||(answer == "scissors") ) {
-        determineWinner(computerChoice, answer)
-    } else{
-        println("$answer is not an option. Try again")
+    if ((userChoice == "rock") ||(userChoice == "paper")||(userChoice == "scissors") ) {
+        determineWinner(computerChoice, userChoice)
+    } else if(userChoice == ""){
+        println("Please fill something in")
+        choose(computerChoice)
+    }else{
+        println("$userChoice is not an option. Try again")
         choose(computerChoice)
     }
 }
