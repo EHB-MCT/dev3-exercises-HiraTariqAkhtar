b608b0f5759a089d99ac75636a522b7a2e9cde75
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2021-10-27 13:17:33 +0200

    Added challenge four

--- session2/adventure_game/src/main/kotlin/main.kt
+++ session2/adventure_game/src/main/kotlin/main.kt
@@ -12,7 +12,7 @@ fun main(){
 fun challengeOne(friend:String?, user:String?){
     println("You start by going to ${friend}'s house")
     println("Solve the following riddle to continue your search:")
-    val options = setOf("strawberry", "watermelon", "raspberry", "cherry")
+    val options = setOf("strawberry", "watermelon", "raspberry", "cherry", "cherries", "Strawberry", "Watermelon", "Raspberry", "Cherry", "Cherries")
     println("You find a plate on the dining table, it has red spots on it.")
     println("Which fruit do you think ${friend} was eating?")
     println("Note: if you give the wrong answer, your adventure will stop here")
@@ -20,7 +20,7 @@ fun challengeOne(friend:String?, user:String?){
     if (options.contains(answer)){
         println("You guessed it right!")
         println("You find another clue near the terrace door.")
-        println("There, you find a note with an adress of where your friend is being held")
+        println("There, you find a note with an address of where your friend is being held")
         challengeTwo(friend, user)
     } else{
         println("Wrong answer")
@@ -46,22 +46,30 @@ fun rollDice(friend:String?, user:String?){
             challengeThree(friend, user)
     } else {
         println("${userAnswer} is unfortunately the wrong answer")
-        gameOver(friend, user)
+        println("Do you want to guess again?")
+        println("Note: The dice will be thrown again")
+        val ans = readLine()
+        if (ans == "yes" || ans == "Yes"){
+            rollDice(friend, user)
+        }else{
+            gameOver(friend, user)
+        }
     }
 }
 
 fun challengeThree(friend:String?, user:String?){
     println("Your choice is now going to be crucial for ${friend}'s rescue")
     println("What will you choose?")
-    println("Run away, find a distraction or fight?")
+    println("A: run away, B: find a distraction or C: fight?")
     val answer = readLine()
-    if (answer == "run away"){
+    if (answer == "a" || answer == "A" ){
         println("You've let your friend down. What a disloyal friend you are! Shame on you!")
         gameOver(friend, user)
-    } else if(answer == "find a distraction"){
+    } else if(answer == "b" || answer == "B" ){
         distractionChoice(friend, user)
-    } else if(answer == "fight"){
-        println("You chose to fight your enemy")
+    } else if(answer == "c" || answer == "C" ){
+        println("You chose to fight your enemy, but he doesn't want to fight.")
+        println("He has a last challenge for you.")
         println("Get ready for your next challenge")
         challengeFour(friend, user)
     }else{
@@ -73,9 +81,9 @@ fun distractionChoice(friend:String?, user:String?){
     println("You look around to find a distraction")
     println("You see a dog and a painting on the wall")
     println("What will you do?")
-    println("Ask about the painting or throw something at the dog?")
+    println("A: ask about the painting or B: throw something at the dog?")
     val answer = readLine()
-    if (answer == "ask about the painting"){
+    if (answer == "a" || answer == "A" ){
         println("You begin to ask questions about the painting")
         println("While your enemy is answering your questions you carefully go towards your friend ")
         println("You almost reached ${friend} but your enemy saw you")
@@ -83,7 +91,7 @@ fun distractionChoice(friend:String?, user:String?){
         println("You have to predict the roll dice again")
         println("Note: if you get it wrong now, all your chances to rescue your friend will be gone...")
         rollDice(friend, user)
-    } else if(answer == "throw something at the dog"){
+    } else if(answer == "b" || answer == "B" ){
         println("You find a rock on the ground and you throw it at the dog")
         println("The dog gets angry and starts to bark very loudly")
         println("The dog is so out of control that your enemy has to go to him to check what's wrong")
@@ -94,7 +102,27 @@ fun distractionChoice(friend:String?, user:String?){
 }
 
 fun challengeFour(friend:String?, user:String?){
-    
+    println("The challenge you get is very easy")
+    println("You just have to guess the number between 1 and 100")
+    println("NOTE: You'll keep guessing till you find the right number")
+    println("READY, SET, GO!!")
+    val num = (1..100).random()
+    guessNumber(friend, user, num)
+}
+
+fun guessNumber(friend: String?, user: String?, num:Int){
+    println("Which number can it be?")
+    var guess = readLine()?.toInt()
+    if (num == guess){
+        println("${guess} is the right number")
+        success(friend, user)
+    } else if(num < guess!!){
+        println("The actual number is lower, try again")
+        guessNumber(friend, user, num)
+    }else if(num > guess!!){
+        println("The actual number is higher, try again")
+        guessNumber(friend, user, num)
+    }
 }
 
 fun gameOver(friend:String?, user:String?){
