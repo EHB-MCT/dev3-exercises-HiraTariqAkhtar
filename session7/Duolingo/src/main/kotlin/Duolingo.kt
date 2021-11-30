57a2c9bd9abbf74b93966c2da276e7949ebf5fc6
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2021-11-30 17:08:39 +0100

    Completed the play function

--- session7/Duolingo/src/main/kotlin/Duolingo.kt
+++ session7/Duolingo/src/main/kotlin/Duolingo.kt
@@ -1,14 +1,35 @@
 class Duolingo{
     val words = mutableSetOf<Word>(
         Word("maison", "huis", "fr"),
-        Word("coeur", "hart", "fr"),
-        Word("main", "hand", "fr"),
+        Word("fille", "meisje", "fr"),
+        Word("garçon", "jongen", "fr"),
         Word("nom", "naam", "fr"),
-        Word("frère", "broer", "fr"),
         Word("vache", "koe", "fr"),
-        Word("école", "school", "fr")
+        Word("chat", "kat", "fr")
     )
     fun play(){
-    
+        println("Vertaal de volgende Franse woorden naar het Nederlands:")
+        println("Veel succes!!")
+
+        val randomWords = words.shuffled().take(5).toMutableSet()
+        // .shuffled => zodat we niet elke keer 5 dezelfde hebben
+        // .take() => aantal items uit set nemen
+        // toMutableSet => zodat een item kan worden weggehaald
+
+        while (randomWords.isNotEmpty()){
+            val selectedWord = randomWords.random()
+            println(selectedWord.original)
+            val answer = readLine()
+
+            if (answer == selectedWord.translated){
+                randomWords.remove(selectedWord)
+                println("Je antwoord is juist")
+            } else{
+                println("Je antwoord is fout")
+                println("Het juiste antwoord was ${selectedWord.translated}")
+            }
+
+            println("Er blijven nog ${randomWords.count()} woorden over")
+        }
     }
 }
