8035e703e321b6bfdd331e2fe68d19bddb5eb4f1
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2021-11-30 15:46:37 +0100

    Made the Class Word and added a few words in the class Duolingo

---
+++ session7/Duolingo/src/main/kotlin/Duolingo.kt
@@ -0,0 +1,14 @@
+class Duolingo{
+    val words = mutableSetOf<Word>(
+        Word("maison", "huis", "fr"),
+        Word("coeur", "hart", "fr"),
+        Word("main", "hand", "fr"),
+        Word("nom", "naam", "fr"),
+        Word("frère", "broer", "fr"),
+        Word("vache", "koe", "fr"),
+        Word("école", "school", "fr")
+    )
+    fun play(){
+    
+    }
+}
