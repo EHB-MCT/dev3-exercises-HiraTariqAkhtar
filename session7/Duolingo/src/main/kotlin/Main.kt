71600b7fb2e4b1cf92aa1c182190192edf84495b
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2021-12-07 14:49:01 +0100

    Ask roundSize and language to player

--- session7/Duolingo/src/main/kotlin/Main.kt
+++ session7/Duolingo/src/main/kotlin/Main.kt
@@ -1,4 +1,10 @@
 fun main(){
     val game = Duolingo()
-    game.play()
+    println("Hoeveel woorden zou je willen vertalen?")
+    println("Kies een getal tussen 5 en 10")
+    val roundSize = readLine()!!.toInt()
+    println("Kies taal:")
+    println("eng of fr?")
+    val lang = readLine().toString()
+    game.play(roundSize,lang)
 }
