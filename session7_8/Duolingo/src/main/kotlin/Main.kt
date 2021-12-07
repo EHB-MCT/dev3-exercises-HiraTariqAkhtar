f6c3cb88f191292a2a66c50fca1091d4ce334ad4
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2021-12-07 16:24:03 +0100

    Throw an error if you choose another language than eng or fr

--- session7_8/Duolingo/src/main/kotlin/Main.kt
+++ session7_8/Duolingo/src/main/kotlin/Main.kt
@@ -3,6 +3,11 @@ fun main(){
     println("eng of fr?")
     val lang = readLine().toString()
 
+    // enkel keuze tussen eng en fr => al de rest krijgt een error
+    if (lang != "fr" && lang != "eng"){
+        throw error("${lang} is geen optie, je hebt de keuze tussen fr en eng")
+    }
+
     println("Hoeveel woorden zou je willen vertalen?")
     println("Kies een getal tussen 5 en 10")
     val roundSize = readLine()!!.toInt()
