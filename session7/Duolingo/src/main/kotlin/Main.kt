222a8eb3e5909d11664c6735515c2159ae519b8d
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2021-12-07 15:28:31 +0100

    Changed the order of asking language and roundSize

--- session7/Duolingo/src/main/kotlin/Main.kt
+++ session7/Duolingo/src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
 fun main(){
-    println("Hoeveel woorden zou je willen vertalen?")
-    println("Kies een getal tussen 5 en 10")
-    val roundSize = readLine()!!.toInt()
     println("Kies taal:")
     println("eng of fr?")
     val lang = readLine().toString()
+
+    println("Hoeveel woorden zou je willen vertalen?")
+    println("Kies een getal tussen 5 en 10")
+    val roundSize = readLine()!!.toInt()
+
     val game = Duolingo(roundSize,lang)
     game.play()
 }
